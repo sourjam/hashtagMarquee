6797110c30f5859224cfab403bf7ec177c7b7d0a
Jim Yang <jimyyang@gmail.com>
2017-10-08 12:12:21 -0400

    simple input for now

--- client/src/client.js
+++ client/src/client.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class SimpleInput extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {}
+    this.state.userInput = 'Enter hashtag'
+  }
+  updateInput(event) {
+    this.setState({userInput: event.target.value})
+  }
+  render() {
+    return (
+      <input onChange={this.updateInput.bind(this)} type="text" value={this.state.userInput}></input>
+    )
+  }
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super();
   }
   render() {
     return (
-      <div>Hello React</div>
+      <div>
+        <h3>Hello React</h3>
+        <SimpleInput />
+      </div>
     )
   }
 }
